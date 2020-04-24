a6eba6127849006b13c9d97c68eba07542f0a57b
Alejandro <acuellarh@gmail.com>
2020-04-24 07:02:29 -0500

    Edit feature working, edit and updateComponentValue created for that

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
     super(props); 
     this.state = {
       timers: [
-        { id: 1, title: "Leer", project: "React" },    
-        { id: 2, title: "imprimir", project: "Makeit" }
+        { id: 1, title: "Leer", project: "React", edit:true },    
+        { id: 2, title: "imprimir", project: "Makeit", edit:false }
       ],
       errors: {
         title: false,
@@ -82,9 +82,33 @@ class App extends Component {
     console.log("should go to edit mode")
   }
 
-  edit(event){
+  edit (id_clicked, event)  {     
+    const newTimers = this.state.timers.map(timer => {
+      if (timer.id === id_clicked){
+        timer.edit = !timer.edit
+      }
+      return timer
+    })    
+    this.setState({
+      timers: newTimers   
+    })  
+  }
+
+  updateComponentValue (id_clicked, event)  {    
     event.preventDefault()
-    console.log("should go to edit mode")
+    const title = event.target['title'].value
+    const project = event.target['project'].value
+    const newTimers = this.state.timers.map(timer => {
+      if (timer.id === id_clicked){
+        timer.edit = !timer.edit
+        timer.title = title
+        timer.project = project
+      }
+      return timer
+    })    
+    this.setState({
+      timers: newTimers   
+    })  
   }
 
   render() {
@@ -119,25 +143,50 @@ class App extends Component {
       
         {this.state.timers.map ((timer, index) =>
           <div className="row" key={index}>
-            <div className="col-sm-4 col-sm-offset-4">    
-              <form>
-                <Card>
-                  <Card.Body>
-                  <Card.Title><strong>{timer.title}</strong></Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">{timer.project}</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>           
-                  <Card.Text className="btn-align ">
-                    <Button variant="light"  > <FaTrashAlt  color='' size='1.0em' /> </Button> 
-                    <Button variant="light" onClick={this.edit.bind(this)} > <FaPencilAlt color='' size='1.0em' /> </Button> 
-                  </Card.Text>           
-                  <Button variant="success" block > Start</Button>          
-                </Card.Body>      
-                </Card>
-              </form>  
-            </div>{/* div col */}
+            {timer.edit ?
+              <div className="col-sm-4 col-sm-offset-4">    
+                <form>
+                  <Card>
+                    <Card.Body>
+                    <Card.Title><strong>{timer.title}</strong></Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{timer.project}</Card.Subtitle>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>           
+                    <Card.Text className="btn-align ">
+                      <Button variant="light"  > <FaTrashAlt  color='' size='1.0em' /> </Button> 
+                      <Button variant="light" onClick={this.edit.bind(this, timer.id)} > <FaPencilAlt color='' size='1.0em' /> </Button> 
+                    </Card.Text>           
+                    <Button variant="success" block > Start</Button>          
+                  </Card.Body>      
+                  </Card>
+                </form>  
+              </div>
+              :              
+              <div className="col-sm-4 col-sm-offset-4">    
+                <form onSubmit={this.updateComponentValue.bind(this, timer.id)}>            
+                  <div>
+                    <label htmlFor="title">Title</label>
+                    <input type="text" defaultValue={timer.title} name="title" />
+                  </div>
+                  <div>
+                    <label htmlFor="project">Project</label>
+                    <input type="text" defaultValue={timer.project} name="project" />
+                  </div>
+
+                  <div className="flex-container">
+                    <div className="item">
+                      <button type="submit" className="btn btn-success btn2" >Update</button>              
+                    </div>
+                    <div className="item">
+                      <button type="submit" className="btn btn-danger btn2" onClick={this.edit.bind(this, timer.id)}>Cancel</button>                  
+                    </div>
+                  </div> 
+                    
+                </form>  
+              </div>
+            }
           </div>  
         )}
 
